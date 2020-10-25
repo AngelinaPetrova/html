7ecb2503dde474abbae37c94dc794df23e9dcce1
AngelinaPetrova <angelina.petrova35@gmail.com>
2020-10-25 14:04:48 +0000

    Coment

--- js/index.js
+++ js/index.js
@@ -1,21 +1,2 @@
-alert('Hello JavaScript');
-let productPrice = 200 + 1;
-alert(productPrice);
-productPrice = productPrice * 1.1;
-alert(productPrice);
-let userName = prompt('Enter your name');
-alert(userName);
-const userAge = Number(prompt("Enter your age"));
-alert('Your age is ' + userAge);
-const nextAge = add(userAge, 1);
-alert('Next year you will be ' + nextAge);
-
-if (userAge < 18) {
-    alert('You are not underaged');
-} else{
-    alert('You are adult');
-}
-function add(a, b) {
-    const sum = a + b;
-    return sum;
-}
+const h1 = document.querySelector('h1');
+h1.style.color = '#101C73 ';
