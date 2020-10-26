0ae14ab35ad9cd59e6319ab62d9353a1830bbde5
AngelinaPetrova <angelina.petrova35@gmail.com>
2020-10-26 12:25:51 +0000

    Co

--- js/calculator.js
+++ js/calculator.js
@@ -1,20 +1,39 @@
-document.querySelectorAll('.calculator .digits button')
+document.querySelectorAll('.calculator .digits')
     .forEach( button => button.addEventListener('click', digitPressed) );
 function digitPressed(ev) {
     const btn = ev.target;
-    const display = document.querySelector('.display');
-    display.value += btn.innerText; 
+    const input = document.querySelector('.input');
+    input.value += btn.innerText; 
 }
-document.querySelectorAll('.calculator .operators button')
+document.querySelectorAll('.calculator .operators')
     .forEach( button => button.addEventListener('click', operatorPressed) );
 function operatorPressed(ev) {
     const btn = ev.target;
-    const display = document.querySelector('.display');
-    display.value += btn.innerText; 
+    const input = document.querySelector('.input');
+    input.value += btn.innerText; 
 }
 
 document.querySelector('.calculator .eq').addEventListener('click', eqPressed)
 function eqPressed() {
-    const display = document.querySelector('.display');
-    display.value = eval(display.value);
+    const input = document.querySelector('.input');
+    input.value = eval(input.value);
 }
+document.querySelector('.calculator .clean').addEventListener('click', cleanPressed)
+function cleanPressed() {
+    const input = document.querySelector('.input');
+    input.value = '';
+}
+function decimal (argument) {
+    const input = document.querySelector('.input');
+		if (inputResult) 
+		{
+			input = "0.";
+			inputResult = false;
+		}
+		else
+		{
+			if (input.indexOf(".") == -1)
+				inputResult += ".";
+		}
+		document.querySelector('.input') = input;
+};
